520dbfc55161ad2068f05266a9e0235f594ba9cb
Theodor Borgman <tb222yz@student.lnu.se>
2022-09-07 08:36:23 +0200

    Add Base64 feature

---
+++ src/commands/tools/base64Message.js
@@ -0,0 +1,19 @@
+const {SlashCommandBuilder} = require('discord.js')
+const { execute } = require('../../events/client/ready') 
+
+const reverseCommand = new SlashCommandBuilder()
+    .setName('base64')
+    .setDescription('sends back message as base64.')
+    .addStringOption(option =>
+      option.setName('input')
+        .setDescription('The input converted to B64')
+        .setRequired(true))
+
+module.exports = {
+  data: reverseCommand,
+  async execute(interaction, client) {
+    const message = await interaction.options.getString("input")
+    const newMessage = Buffer.from(message).toString('base64')
+    await interaction.reply(newMessage)
+  }
+}
