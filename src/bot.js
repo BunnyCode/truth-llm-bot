2432bdb80cd56e0329616bb9d9b363bba4b361b8
Theodor Borgman <theodor.borgman@gmail.com>
2024-02-18 13:19:53 +0100

    Add healt check endpoint

--- src/bot.js
+++ src/bot.js
@@ -3,13 +3,23 @@ dotenv.config();
 const path = require("node:path");
 const { Client, Events, Collection, GatewayIntentBits } = require("discord.js");
 const fs = require("fs");
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
 const deployCommands = require("./functions/handlers/handleCommands");
 
-console.log("Starting TruGPT Bot...");
 const token = process.env.DISCORD_TOKEN;
 
 try {
+  console.log("Starting TruGPT Bot...");
   deployCommands().then(() => {
+    // Health check endpoint
+    app.get("/health", (req, res) => {
+      res.status(200).send("OK");
+    });
+
     // Create a new client instance
     const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -79,6 +89,12 @@ try {
 
     // Log in to Discord with your client's token
     client.login(token);
+
+    // Start Health Check Server
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   });
 } catch (error) {
   console.error(error);
