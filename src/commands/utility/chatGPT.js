fde768be548ef8b3273078c9e34833e339c0f5f0
Fredrik Jurgell <fj222qp@student.lnu.se>
2024-02-20 10:25:57 +0100

    Add body parameters

--- src/commands/utility/chatGPT.js
+++ src/commands/utility/chatGPT.js
@@ -30,7 +30,12 @@ module.exports = {
         messages: [{
           role: "user",
           content: message
-        }]
+        }],
+        temperature: 0.7,
+        max_tokens: 1000,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0
       })
     });
 
