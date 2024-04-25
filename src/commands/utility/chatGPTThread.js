fe9554d81895ee558c25a8137dedbc38464095cf
Theodor Borgman <theodor.borgman@adway.ai>
2024-04-26 00:29:40 +0200

    Cleanup

--- src/commands/utility/chatGPTThread.js
+++ src/commands/utility/chatGPTThread.js
@@ -1,21 +1,30 @@
 const { SlashCommandBuilder } = require('discord.js');
 const path = require('path');
 
-const discordFunctions = require(path.join(__dirname, '../../helpers/discordFunctions'));
-const dF = new discordFunctions;
+const discordFunctions = require(path.join(
+  __dirname,
+  '../../helpers/discordFunctions',
+));
+const dF = new discordFunctions();
 const [searchInternet, searchArticle] = require('../../functions/scrape/scaper');
 global.searchInternet = searchInternet;
 global.searchArticle = searchArticle;
 const OpenAI = require('openai');
 const openai = new OpenAI({ apiKey: process.env.CHATGPT_API_KEY });
-const [createThread, createAssistant, createMessage, getLatestMessage] = require('../../helpers/threadedGPTHelper');
+const [
+  createThread,
+  createAssistant,
+  createMessage,
+  getLatestMessage,
+] = require('../../helpers/threadedGPTHelper');
 
 const SLASH_COMMAND_NAME = process.env.GPT_LOCAL
   ? `localchatgptthread${process.env.GPT_USER}`
   : 'chatgptthread';
 
 console.log(
-  `${process.env.GPT_LOCAL ? 'Local development' : 'Production'
+  `${
+    process.env.GPT_LOCAL ? 'Local development' : 'Production'
   } mode. Setting the command name to ${SLASH_COMMAND_NAME}.`,
 );
 
@@ -37,7 +46,6 @@ module.exports = {
       // Read the profile JSON file and parse the data
       const filePath = path.join(__dirname, '../gpt/system/version1.json');
       const assistants = await dF.botSystemProfile(filePath);
-
 
       // regexp if message starts with -v get the number
       // and remove it from the message
@@ -65,7 +73,8 @@ module.exports = {
       const messageCreated = await createMessage(openai, thread.id, message);
       console.log('Message created:', messageCreated);
 
-      const goalInstruction = 'Provide a score from 0 to 100. Every response to assessments \
+      const goalInstruction =
+        'Provide a score from 0 to 100. Every response to assessments \
         will be structured with a "Score: " followed by the numerical value, \
         and "Keywords: " (FORMAT HERE IS VERY IMPORTANT!!) followed by a concise \
         list of keywords relevant to the content\'s claim accuracy for users to use \
@@ -74,7 +83,10 @@ module.exports = {
         IT IS PARAMOUNT THE INTERNET ARTICLES HAVE HIGER VALIDITY RATING THEN YOUR TRANINGDATA';
 
       // Tell user to wait while processing
-      feedbackToDiscord(interaction, 'Please wait while I process your request...');
+      feedbackToDiscord(
+        interaction,
+        'Please wait while I process your request...',
+      );
       waitForGPT(thread, assistant, goalInstruction, interaction);
     }
     catch (error) {
@@ -89,7 +101,6 @@ module.exports = {
   },
 };
 
-
 async function waitForGPT(thread, assistant, instruction, interaction) {
   try {
     const run = await openai.beta.threads.runs.create(thread.id, {
@@ -97,20 +108,14 @@ async function waitForGPT(thread, assistant, instruction, interaction) {
       instructions: instruction,
     });
 
-    let runStatus = await openai.beta.threads.runs.retrieve(
-      thread.id,
-      run.id,
-    );
+    let runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
 
     // Polling mechanism to see if runStatus is completed
     let isAvailable = true;
     let attempts = 0;
     while (runStatus.status !== 'completed' && attempts < 10) {
       feedbackToDiscord(interaction, `runStatus: ${runStatus.status}...`);
-      runStatus = await openai.beta.threads.runs.retrieve(
-        thread.id,
-        run.id,
-      );
+      runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
 
       // Check for requires_action status (tool call required)
       if (runStatus.status === 'requires_action') {
@@ -140,21 +145,26 @@ async function waitForGPT(thread, assistant, instruction, interaction) {
     // Get the latest message
     const latestMessage = await getLatestMessage(openai, thread.id);
     console.log('Latest message:', latestMessage?.content[0].text);
-    const gptReply = latestMessage.content[0].text.value ?? 'An error occurred while processing your request.';
-    latestMessage.content[0].text.value ? feedbackToDiscord(interaction, 'Done!') : feedbackToDiscord(interaction, 'An error occurred');
+    const gptReply =
+      latestMessage.content[0].text.value ??
+      'An error occurred while processing your request.';
+    latestMessage.content[0].text.value
+      ? feedbackToDiscord(interaction, 'Done!')
+      : feedbackToDiscord(interaction, 'An error occurred');
     interaction.followUp(gptReply);
     // Ask bot what article url to use""
     // await createMessage(openai, thread.id, 'Please provide the article url you would like to use.');
   }
   catch (error) {
     console.error('Error in waitForGPT:', error);
-    await interaction.followUp('An error occurred while processing your request.');
+    await interaction.followUp(
+      'An error occurred while processing your request.',
+    );
   }
 }
 
 async function useTool(runStatus, thread, run, interaction) {
-  const toolCalls =
-    runStatus.required_action.submit_tool_outputs.tool_calls;
+  const toolCalls = runStatus.required_action.submit_tool_outputs.tool_calls;
   const toolOutputs = [];
 
   console.log(toolCalls);
@@ -162,29 +172,22 @@ async function useTool(runStatus, thread, run, interaction) {
   for (const toolCall of toolCalls) {
     const functionName = toolCall.function.name;
 
-    console.log(
-      `This question requires us to call a function: ${functionName}`,
-    );
+    console.log(`This question requires us to call a function: ${functionName}`);
     await feedbackToDiscord(interaction, `Using function: ${functionName}`);
 
     const args = JSON.parse(toolCall.function.arguments);
 
-    // const argsArray = Object.keys(args).map((key) => args[key]);
-
     // Dynamically call the function with arguments
     const output = await global[functionName].apply(null, [args]);
-    // console.log('output:', output);
     toolOutputs.push({
       tool_call_id: toolCall.id,
       output: output,
     });
   }
   // Submit tool outputs
-  await openai.beta.threads.runs.submitToolOutputs(
-    thread.id,
-    run.id,
-    { tool_outputs: toolOutputs },
-  );
+  await openai.beta.threads.runs.submitToolOutputs(thread.id, run.id, {
+    tool_outputs: toolOutputs,
+  });
 }
 
 async function feedbackToDiscord(interaction, message) {
@@ -193,6 +196,8 @@ async function feedbackToDiscord(interaction, message) {
   }
   catch (error) {
     console.error('Error in feedbackToDiscord:', error);
-    await interaction.followUp('there was an error on our end. Please try again.');
+    await interaction.followUp(
+      'there was an error on our end. Please try again.',
+    );
   }
 }
